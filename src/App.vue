db5671a7064b9d34b461eba71b7c50e621f93028
rendipilot <linahonduras@orotonmail.com>
2025-04-01 11:06:26 +0700

    add cat card component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,64 @@
-<script>
-</script>
-
 <template>
-  <div>
-    <p class="text-2xl text-blue-500 font-bold">Hello world</p>
+  <div class="w-full h-14 flex bg-gradient-to-r from-yellow-400 to-yellow-600">
+    <h3 class="text-2xl text-black w-max ml-4 my-auto font-bold">Cat World</h3>
+  </div>
+
+  <div class="flex">
+    <div class=" my-10 w-11/12 mx-auto">
+      <button
+        @click="catFunction"
+        class="bg-blue-500 p-3 rounded text-white font-semibold"
+      >
+      {{ isShowing ? 'Hide' : 'Show cat' }}
+      </button>
+      <div class="mt-5 grid grid-cols-4 gap-4">
+        <CatCard v-for="(cat, index) in cats" :key="index" :imageUrl="cat.url" />
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+import { defineComponent, ref } from "vue";
+import CatCard from "./components/CatCard.vue";
 
+export default defineComponent({
+  components: {
+    CatCard
+  },
+  setup() {
+    const cats = ref([]);
+    const isShowing = ref(false);
+
+
+    const fetchCats = async () => {
+      try {
+        const res = await fetch(
+          "https://api.thecatapi.com/v1/images/search?limit=10"
+        );
+        const data = await res.json();
+
+        cats.value = data;
+      } catch (error) {
+        console.error("Error fetching cat data:", error);
+      }
+    };
+
+    const catFunction = () => {
+      if (isShowing.value){
+          cats.value = []
+      }else{
+        fetchCats()
+      }
+
+      isShowing.value = !isShowing.value
+    }
+
+    return {
+      cats,
+      catFunction,
+      isShowing,
+    };
+  },
+});
+</script>
---
+++ src/components/CatCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card bg-base-100 w-72 shadow-sm">
+    <img :src="imageUrl" alt="Cat image" class="w-full h-64 object-cover" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
